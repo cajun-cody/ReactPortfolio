c4f40a83372bb70dc845a737c52fafde31e39b1c
Cody <cemt5462@gmail.com>
2023-09-08 10:56:22 -0500

    Made some styling changes to portfolio projects but still needs work on text. Finished styling skills section and added life goal block with bouncing animation.

--- src/Components/Projects/portfolio.jsx
+++ src/Components/Projects/portfolio.jsx
@@ -3,7 +3,7 @@ import './portfolio.css';
 
 import project1 from '../../Assets/project1.jpg';
 import MusicLibrary from '../../Assets/MusicLibrary.png';
-
+import backwardCajun from '../../Assets/backwardCajun.jpg';
 
 const Portfolio = () => {
     return ( 
@@ -16,18 +16,22 @@ const Portfolio = () => {
                 <div class="portfolio-layer">                
                     <p>A music library for video game soundtracks. Ability to add and delete songs as well as search for other songs in the database.</p>
                     <p>Tech Stack: Python/Django, MySQL, JavaScript/React</p>
-                    <a href="https://github.com/cajun-cody/Music_Library_Frontend"><i class='bx bxl-github'></i></a>
-                    <a href="https://github.com/cajun-cody/Music_Library_Backend"><i class='bx bxl-github'></i></a>
+                    <div class="project-links">
+                        <a href="https://github.com/cajun-cody/Music_Library_Frontend"><i class='bx bxl-github'></i></a>
+                        <a href="https://github.com/cajun-cody/Music_Library_Backend"><i class='bx bxl-github'></i></a>
+                    </div>
                 </div>
             </div>
             <div class="portfolio-box">
-                <img src={project1} alt=""/>
-                <h4 class="portfolio-title">Capstone</h4>
+                <img src={backwardCajun} alt=""/>
+                <h4 class="portfolio-title">The Backward Cajun-Capstone</h4>
                 <div class="portfolio-layer">                
-                    <p>A music library for video game soundtracks. Ability to add and delete songs as well as search for other songs in the database.</p>
+                    <p>A digital recipe application that allows users to create accounts and add, modify and delete their favorite recipes. Users can also set their desired serving size to adjust the recipe for any recipe to fit any occasion.</p>
                     <p>Tech Stack: Python/Django, MySQL, JavaScript/React</p>
-                    <a href="https://github.com/cajun-cody/Music_Library_Frontend"><i class='bx bxl-github'></i></a>
-                    <a href="https://github.com/cajun-cody/Music_Library_Backend"><i class='bx bxl-github'></i></a>
+                    <div class="project-links">
+                        <a href="https://github.com/cajun-cody/Music_Library_Frontend"><i class='bx bxl-github'></i></a>
+                        <a href="https://github.com/cajun-cody/Music_Library_Backend"><i class='bx bxl-github'></i></a>
+                    </div>                   
                 </div>
             </div>
             <div class="portfolio-box">
@@ -35,25 +39,40 @@ const Portfolio = () => {
                 <h4 class="portfolio-title">RouxTube Full Stack-Python</h4>
                 <div class="portfolio-layer">
                     <p>Video streaming site using youtube api. Upon login the site displays a set of cajun cooking videos. There is also the ability to search for any type of video as well as leave comments and reply to ther comments. </p>
-                    <a href="https://github.com/cajun-cody/YouTube_Clone"><i class='bx bxl-github'></i></a>
+                    <div class="project-links">
+                        <a href="https://github.com/cajun-cody/YouTube_Clone"><i class='bx bxl-github'></i></a>
+                    </div>    
                 </div>
             </div>
             <div class="portfolio-box">
                 <img src={project1} alt=""/>
                 <h4 class="portfolio-title">Book Nook Full Stack-C#</h4>
                 <div class="portfolio-layer">              
-                    <p>Video streaming site using youtube api. Upon login the site displays a set of cajun cooking videos. There is also the ability to search for any type of video as well as leave comments. </p>
-                    <a className="portfolio-link" href="https://github.com/cajun-cody/BookNook_Frontend/tree/main/Frontend"><i class='bx bxl-github'></i></a>
-                    <a className="portfolio-link" href="https://github.com/cajun-cody/BookNook_Backend"><i class='bx bxl-github'></i></a>
+                    <p>Book review application using Google Books API. Users can log in and leave reviews for books as well as save thier favorite books to review later.</p>
+                    <div class="project-links">
+                        <a className="portfolio-link" href="https://github.com/cajun-cody/BookNook_Frontend/tree/main/Frontend"><i class='bx bxl-github'></i></a>
+                        <a className="portfolio-link" href="https://github.com/cajun-cody/BookNook_Backend"><i class='bx bxl-github'></i></a>
+                    </div>
                 </div>
             </div>
             <div class="portfolio-box">
                 <img src={project1} alt=""/>
                 <h4 class="portfolio-title">Lemonade Stand- C#</h4>
                 <div class="portfolio-layer">
-                    <p>Backend terminal game in C#. This game allows a user to shop for products to make lemonage and sell it to customers. The experience even takes into account weather.</p>
-                    <p></p>
-                    <a href="https://github.com/cajun-cody/Lemonade_Stand"><i class='bx bxl-github'></i></a>
+                    <p>Backend terminal game in C#. This game allows a user to shop for products to make lemonage and sell it to customers. The experience even takes into account weather. Players can play for a selected amount of time and each day a total sales is displayed along with a grand total at the end.</p>
+                    <div class="project-links">
+                        <a href="https://github.com/cajun-cody/Lemonade_Stand"><i class='bx bxl-github'></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="portfolio-box">
+                <img src={project1} alt=""/>
+                <h4 class="portfolio-title">Ecomm Web Scrapper</h4>
+                <div class="portfolio-layer">
+                    <p>Simple web scrapper for an ecommerce site that pulls data and sorts it to a csv file. Project uses Selenium webdriver and Python.</p>
+                    <div class="project-links">
+                        <a href="https://github.com/cajun-cody/SimpleWebScraper/blob/main/script.py"><i class='bx bxl-github'></i></a>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Components/SkillsAndTechnologies/SkillsAndTechnologies.jsx
+++ src/Components/SkillsAndTechnologies/SkillsAndTechnologies.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React,{useState, useEffect}  from "react";
 import './SkillsAndTechnologies.css';
 
+
 const SkillsAndTechnologies = () => {
+
+    const [animateLifeGoal, setAnimateLifeGoal] = useState(false);
+
+    useEffect(() => {
+        // Trigger the animation after a delay or when a certain event occurs
+        const timer = setTimeout(() => {
+            setAnimateLifeGoal(true);
+        }, 1000); // Delay for 1 second (adjust as needed)
+
+        // Clean up the timer to prevent memory leaks
+        return () => clearTimeout(timer);
+    }, []);
+
+
     return ( 
         <section class="services" id="services">
         <h2 class="heading">Skills <span>& Technologies</span></h2>
@@ -9,22 +24,23 @@ const SkillsAndTechnologies = () => {
             <div class="services-box">
                 <i class='bx bx-code-alt' ></i>
                 <h3>Backend Development</h3>
-                <p>Skills and Technologies</p>
                 <ul>
                     <li>C#</li>
                     <li>Python</li>
                     <li>ASP.NET MVC Core</li>
                     <li>ASP.NET Web API</li>
                     <li>LINQ</li>
+                    <li>Django</li>
                     <li>Django Rest API</li>
                 </ul>
             </div>
             <div class="services-box">
-                <i class='bx bx-search-alt'></i>
+                <i class='bx bxl-react'></i>
                 <h3>Frontend Development</h3>
-                <p>Skills and Technologies</p>
                 <ul>
                     <li>JavaScript</li>
+                    <li>React.js</li>
+                    <li>JSON</li>
                     <li>HTML</li>
                     <li>CSS</li>
                     <li>SCSS</li>
@@ -34,18 +50,18 @@ const SkillsAndTechnologies = () => {
             <div class="services-box">
                 <i class='bx bx-purchase-tag-alt' ></i>
                 <h3>Support Technologies</h3>
-                <p>Skills and Technologies</p>
                 <ul>
                     <li>Git/Github</li>
                     <li>Visual Studio</li>
                     <li>VS Code</li>
                     <li>Postman</li>
                     <li>MySQL</li>
+                    <li>SQL</li>
                     <li>Figma</li>
                 </ul>
             </div>
-
         </div>
+        <div class={`life-goal ${animateLifeGoal ? 'animate' : ''}`}>Life Goals-- Learn Something New Everyday!!</div>
     </section>
      );
 }
